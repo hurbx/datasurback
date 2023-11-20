1ccd4270987e655577a719ed0c139308f09b8206
hurbx <hugo.urbina@mayor.cl>
2023-11-20 19:27:14 -0300

    fix 3 largo valores

--- comerceapp/models.py
+++ comerceapp/models.py
@@ -5,10 +5,10 @@ from django.db import models
 
 class Data(models.Model):
     date = models.CharField(max_length=50)
-    uf = models.CharField(max_length=10)
-    utm = models.CharField(max_length=10)
-    dolar_obs = models.CharField(max_length=10)
-    euro = models.CharField(max_length=10)
+    uf = models.CharField(max_length=50)
+    utm = models.CharField(max_length=50)
+    dolar_obs = models.CharField(max_length=50)
+    euro = models.CharField(max_length=50)
 
     def __str__(self):
         return f'{self.date} - {self.uf} - {self.utm} - {self.dolar_obs} - {self.euro}'
